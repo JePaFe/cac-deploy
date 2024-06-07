50f7b8151be2c90527e21d5d59655ab9065aaaa4
Jean Paul Ferreira <jepafe@gmail.com>
2024-06-07 16:09:22 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("CaC Deploy");
+  res.send("CaC Deploy!!!");
 });
 
 const PORT = process.env.PORT || 3000;
